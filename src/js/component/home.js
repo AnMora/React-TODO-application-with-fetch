562741b526756d149b01c9efac1f920b2cd5f048
Anmora96 <anmora.2396@gmail.com>
2021-02-22 01:27:18 +0000

    Ejercicio terminado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -19,22 +19,6 @@ export function Home() {
 		setTarea("");
 	};
 
-	fetch(
-		"http://assets.breatheco.de/apis/fake/todos/user/<username>alesanchezr",
-		{
-			method: "PUT",
-			body: JSON.stringify(arrayTareas),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		}
-	).then(resp => {
-		console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
-		console.log(resp.status); // el código de estado = 200 o código = 400 etc.
-		console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
-		return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
-	});
-
 	const borrarTarea = id => {
 		//console.log(id);
 		for (let i = 0; i < arrayTareas.length; i++) {
@@ -45,6 +29,51 @@ export function Home() {
 			}
 		}
 	};
+
+	const postData = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/daemoniun", {
+			method: "POST",
+			BODY: JSON.stringify([]),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+				console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+				console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+				return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch(error => {
+				//manejo de errores
+				console.log(error);
+			});
+	};
+
+	const getData = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/daemoniun", {
+			method: "GET",
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(response => {
+				console.log(response.ok);
+				console.log(response.status);
+				console.log(response.text);
+				return response.json();
+			})
+			.then(data => {
+				console.log(data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
+	getData();
+	postData();
 
 	return (
 		<div className="container">
